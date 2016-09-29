e101508bdcf28eb00eed5f54f8c97c967a8d850d
YannC <yann.chiousse@gmail.com>
2016-09-29 14:19:53 +0200

    ModifYann

--- hippodrome/Fgeneral.Designer.cs
+++ hippodrome/Fgeneral.Designer.cs
@@ -44,10 +44,12 @@
             this.dgCourses = new System.Windows.Forms.DataGridView();
             this.tabgeneral = new System.Windows.Forms.TabControl();
             this.tabCheval = new System.Windows.Forms.TabPage();
+            this.LblYannMoto = new System.Windows.Forms.Label();
             this.tabCourse.SuspendLayout();
             this.gbDetailCourse.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgCourses)).BeginInit();
             this.tabgeneral.SuspendLayout();
+            this.tabCheval.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabCourse
@@ -203,6 +205,7 @@
             // 
             // tabCheval
             // 
+            this.tabCheval.Controls.Add(this.LblYannMoto);
             this.tabCheval.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabCheval.Location = new System.Drawing.Point(4, 28);
             this.tabCheval.Name = "tabCheval";
@@ -211,6 +214,16 @@
             this.tabCheval.TabIndex = 0;
             this.tabCheval.Text = "Les chevaux";
             this.tabCheval.UseVisualStyleBackColor = true;
+            // 
+            // LblYannMoto
+            // 
+            this.LblYannMoto.AutoSize = true;
+            this.LblYannMoto.Font = new System.Drawing.Font("AR BLANCA", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblYannMoto.Location = new System.Drawing.Point(708, 70);
+            this.LblYannMoto.Name = "LblYannMoto";
+            this.LblYannMoto.Size = new System.Drawing.Size(195, 20);
+            this.LblYannMoto.TabIndex = 0;
+            this.LblYannMoto.Text = "MOOOOOTTTTTTTOOOOOO";
             // 
             // Fgeneral
             // 
@@ -226,6 +239,8 @@
             this.gbDetailCourse.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgCourses)).EndInit();
             this.tabgeneral.ResumeLayout(false);
+            this.tabCheval.ResumeLayout(false);
+            this.tabCheval.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -248,6 +263,7 @@
         private System.Windows.Forms.DataGridView dgCourses;
         private System.Windows.Forms.TabControl tabgeneral;
         private System.Windows.Forms.TabPage tabCheval;
+        private System.Windows.Forms.Label LblYannMoto;
 
     }
 }
